6bdb8ac3cab363e272cd7fd7041c40d289b2cc4b
kohyun821 <74554290+kohyun821@users.noreply.github.com>
2023-08-01 21:55:01 +0900

    [unrated] Title: 수 조작하기 1, Time: 0.98 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+       for(char ch : control.toCharArray()){
+            switch (ch){
+                case 'w':
+                    n+=1;
+                    break;
+                case 's':
+                    n-=1;
+                    break;
+                case 'd':
+                    n+=10;
+                    break;
+                case 'a':
+                    n-=10;
+                    break;
+            }
+        }
+        return n;
+    }
+}
