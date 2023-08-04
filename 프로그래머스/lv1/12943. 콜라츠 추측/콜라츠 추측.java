8c8fb5abf2b26ab75ca631b34dcf611891497b1c
kohyun821 <74554290+kohyun821@users.noreply.github.com>
2023-08-04 11:46:25 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        while(true){
+            if(num==1){
+                break;
+            }
+            else{
+                if(num%2==0){
+                    num = num/2;
+                }
+                else{
+                    num = (num*3) +1;
+                }
+                answer++;
+                if(answer>=500){
+                    answer = -1;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
